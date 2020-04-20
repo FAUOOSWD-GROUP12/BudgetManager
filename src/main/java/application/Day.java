b6be8c299bbb75fcfe2b4fa0fc6d282d43100e02
Curtis Woodworth <woodworthc10@gmail.com>
2020-04-20 14:51:48 -0400

    added functionality to day class

--- src/main/java/application/Day.java
+++ src/main/java/application/Day.java
@@ -1,9 +1,51 @@
 package main.java.application;
 
-public class Day{
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public static void main(String[]args){
-        System.out.println("Hello World");
+public class Day {
+    public Day() {
+        purchases = new HashMap<>();
+        categories = new ArrayList<>();
+    }
+
+    public void addItem(String category, Item itemPurchased) {
+        if(this.purchases.containsKey(category)){
+            ArrayList<Item> categoryList = this.purchases.get(category);
+            categoryList.add(itemPurchased);
+            this.categories.add(category);
+        } else{
+            ArrayList<Item> newItem = new ArrayList<>();
+            newItem.add(itemPurchased);
+            this.purchases.put(category, newItem);
+            this.categories.add(category);
         }
-	
+    }
+
+    public ArrayList<String> getCategories() {
+        return (ArrayList<String>) categories.clone();
+    }
+
+    public boolean purchasesIsEmpty() {
+        return this.purchases.isEmpty();
+    }
+
+    public boolean categoriesIsEmpty(){
+        return this.categories.isEmpty();
+    }
+
+    public String toString(){
+        return purchases.toString();
+    }
+    private HashMap<String, ArrayList<Item>> purchases;
+    private ArrayList<String> categories;
+
+    public static void main(String[] args) {
+        Day dayTest = new Day();
+        System.out.println(dayTest.purchasesIsEmpty());
+        dayTest.addItem("A", new Item("food", 20));
+        System.out.println(dayTest.purchasesIsEmpty());
+        dayTest.addItem("A", new Item("water", 5));
+        System.out.println(dayTest.toString());
+    }
 }
--- src/main/java/application/Item.java
+++ src/main/java/application/Item.java
@@ -1,6 +1,20 @@
 package main.java.application;
 
 public class Item{
-	
-	
+
+    public Item(String aName, float aPrice){
+        this.name = aName;
+        this.price = aPrice;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+	private float price;
 }
