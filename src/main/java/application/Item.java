008911a1c9150e70b1cdb67dea267631a9c60e03
Curtis Woodworth <woodworthc10@gmail.com>
2020-04-26 17:00:31 -0400

    commented Item.java

--- src/main/java/application/Item.java
+++ src/main/java/application/Item.java
@@ -1,16 +1,32 @@
 package main.java.application;
 
+/**
+ * Item Object will store an items name and price value.
+ * To be used when a new purchase is made.
+ */
 public class Item {
-
+    /**
+     * Constructor will assign the Item a name and price Value.
+     * @param aName - name of the item purchased.
+     * @param aPrice - price of the item purchased.
+     */
     public Item(String aName, double aPrice) {
         this.name = aName;
         this.price = aPrice;
     }
 
+    /**
+     * getPrice gets the items price.
+     * @return the price of the item.
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * getName() returns the name of the item.
+     * @return String name of the item purchased.
+     */
     public String getName() {
         return name;
     }
@@ -20,6 +36,6 @@ public class Item {
         return name + ' ' + price;
     }
 
-    private String name;
-    private double price;
+    private final String name;
+    private final double price;
 }
