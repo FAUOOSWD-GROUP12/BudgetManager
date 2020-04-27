bf9e3a5ae70b3f053fe24038241b61157c9cd324
Curtis Woodworth <woodworthc10@gmail.com>
2020-04-27 00:15:15 -0400

    check pending

--- src/main/java/gui/CheckSpendingGUI.java
+++ src/main/java/gui/CheckSpendingGUI.java
@@ -1,26 +1,33 @@
 package main.java.gui;
 
+import main.java.application.YearBudget;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CheckSpendingGUI {
+public class CheckSpendingGUI extends JFrame {
 
     private JPanel CheckSpendingPanel;
     private JButton allButton;
+    private JButton viewMonthButton;
+    private JComboBox comboBox1;
 
-    public CheckSpendingGUI() {
+    public CheckSpendingGUI(YearBudget yearReference) {
 
         // Displays window with spending for the entire year
         allButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                new DisplayAllSpendingGUI(yearReference);
             }
         });
-
+        setContentPane(CheckSpendingPanel);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
 
     }
-
 }
